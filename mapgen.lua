a935aa2b04a36601fc770d36000c508db02ac261
Xeno333 <Xeno333@mailfence.com>
2025-06-18 17:42:34 -0500

    performance

--- mapgen.lua
+++ mapgen.lua
@@ -62,18 +62,21 @@ end
 local noiseperiods = {500, 100, 50, 5}
 local noiseamps = {150, 50, 10, 2}
 
-minetest.register_on_generated(function(vmanip, minp, maxp, blockseed)
-	local noises = {}
+local noises = {}
+for i, period in pairs(noiseperiods) do
+	noises[i] = core.get_value_noise_map({
+		offset = 0,
+		scale = noiseamps[i],
+		spread = vector.new(period, period, period),
+		seed = i,
+		octaves = 1,
+	}, {x = 80, y = 80})
+end
+
+core.register_on_generated(function(vmanip, minp, maxp, blockseed)
 	local noisemaps = {}
-	for i, period in ipairs(noiseperiods) do
-		noises[i] = minetest.get_perlin_map({
-			offset = 0,
-			scale = noiseamps[i],
-			spread = vector.new(period, period, period),
-			seed = i,
-			octaves = 1,
-		}, maxp - minp + vector.new(1, 1, 1))
-		noisemaps[i] = noises[i]:get_2d_map_flat({x = minp.x, y = minp.z})
+	for i, noise in pairs(noises) do
+		noisemaps[i] = noise:get_2d_map_flat({x = minp.x, y = minp.z})
 	end
 
 	local data = vmanip:get_data()
@@ -83,81 +86,78 @@ minetest.register_on_generated(function(vmanip, minp, maxp, blockseed)
 	local area = VoxelArea(emin, emax)
 
 	local noise2d_idx = 0
-	local zstride = maxp.x - minp.x + 1
 
-	local current_biomeid = -1
+	local current_biomeid = nil
 	local water_top_cutoff, riverbed_cutoff, top_cutoff, filler_cutoff
 	local node_water_top, node_water, node_riverbed, node_stone, node_top, node_filler
 
 	for z = minp.z, maxp.z do
+		local x_local = 0
 		for x = minp.x, maxp.x do
-			local final_noise2d_idx = noise2d_idx + x - minp.x + 1
-
+			local final_noise2d_idx = noise2d_idx + x_local + 1
 			local height = map_height(x, z)
-			for i, noisemap in pairs(noisemaps) do
+			for _, noisemap in pairs(noisemaps) do
 				height = height + noisemap[final_noise2d_idx]
 			end
 
+			-- Update biome cache if this node is on a different biome
+			local biomedata = core.get_biome_data({x=x, y=height, z=z})
+			if biomedata.biome ~= current_biomeid then
+				current_biomeid = biomedata.biome
+				local biomedef = biome_definitions[current_biomeid]
+
+				water_top_cutoff = biomedef.depth_water_top or default_biome.depth_water_top
+				riverbed_cutoff = biomedef.depth_riverbed or default_biome.depth_riverbed
+				top_cutoff = biomedef.depth_top or default_biome.depth_top
+				filler_cutoff = biomedef.depth_filler or default_biome.depth_filler + top_cutoff
+
+				node_water_top = biomedef.node_water_top or default_biome.node_water_top
+				node_water = biomedef.node_water or default_biome.node_water
+				node_riverbed = biomedef.node_riverbed or default_biome.node_riverbed
+				node_top = biomedef.node_top or default_biome.node_top
+				node_filler = biomedef.node_filler or default_biome.node_filler
+				node_stone = biomedef.node_stone or default_biome.node_stone
+			end
+
 			for y = minp.y, maxp.y do
-				local pos = {x=x, y=y, z=z}
-				local idx = area:indexp(pos)
 				-- Depth is positive when underground, negative when in air
 				local depth = height - y
-
-				-- Update biome cache if this node is on a different biome
-				local biomedata = core.get_biome_data(pos)
-				if biomedata.biome ~= current_biomeid then
-					local biomedef = biome_definitions[biomedata.biome]
-					current_biomeid = biomedata.biome
-
-					water_top_cutoff = biomedef.depth_water_top or default_biome.depth_water_top
-					riverbed_cutoff = biomedef.depth_riverbed or default_biome.depth_riverbed
-					top_cutoff = biomedef.depth_top or default_biome.depth_top
-					filler_cutoff = biomedef.depth_filler or default_biome.depth_filler + top_cutoff
-
-					node_water_top = biomedef.node_water_top or default_biome.node_water_top
-					node_water = biomedef.node_water or default_biome.node_water
-					node_riverbed = biomedef.node_riverbed or default_biome.node_riverbed
-					node_top = biomedef.node_top or default_biome.node_top
-					node_filler = biomedef.node_filler or default_biome.node_filler
-					node_stone = biomedef.node_stone or default_biome.node_stone
-				end
-
 				
 				-- First check, if this is too deep for anything, just place stone
-				if depth > filler_cutoff then
-					data[idx] = node_stone
+				if depth >= filler_cutoff then
+					data[area:indexp({x=x, y=y, z=z})] = node_stone
 				-- Is it underwater?
 				elseif y <= 0 then
-					if depth < 0 and y > -water_top_cutoff then
-						data[idx] = node_water_top
-					elseif depth < 0 then
-						data[idx] = node_water
+					if depth < 0 then
+						if y > -water_top_cutoff then
+							data[area:indexp({x=x, y=y, z=z})] = node_water_top
+						else
+							data[area:indexp({x=x, y=y, z=z})] = node_water
+						end
 					elseif depth < riverbed_cutoff then
-						data[idx] = node_riverbed
+						data[area:indexp({x=x, y=y, z=z})] = node_riverbed
 					else
 						-- Placing stone again just in case the riverbed cutoff isn't as deep as the filler cutoff
-						data[idx] = node_stone
+						data[area:indexp({x=x, y=y, z=z})] = node_stone
 					end
 				else
 					if depth < 0 then
 						-- Just air
 					elseif depth < top_cutoff then
-						data[idx] = node_top
-					elseif depth < filler_cutoff then
-						data[idx] = node_filler
+						data[area:indexp({x=x, y=y, z=z})] = node_top
 					else
-						data[idx] = node_stone
+						data[area:indexp({x=x, y=y, z=z})] = node_filler
 					end
 				end
 			end
+			x_local = x_local + 1
 		end
-		noise2d_idx = noise2d_idx + zstride
+		noise2d_idx = noise2d_idx + 80 -- Add block
 	end
 
 	vmanip:set_data(data)
 	--vmanip:set_param2_data(param2data)
-	minetest.generate_decorations(vmanip)
-	minetest.generate_ores(vmanip)
-	vmanip:calc_lighting()
+	core.generate_decorations(vmanip, minp, maxp)
+	core.generate_ores(vmanip, minp, maxp)
+	vmanip:calc_lighting(minp, maxp)
 end)
